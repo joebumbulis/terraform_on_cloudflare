f382fb55f2e1b08b5bb48457768e2fe21a3129db
Joe Bumbulis <joebumbulis@gmail.com>
2024-11-12 16:30:31 -0600

    adds dns records from terraforming

--- dns.tf
+++ dns.tf
@@ -1,24 +1,27 @@
-# Add a record to the domain
-resource "cloudflare_record" "terraform1" {
-  zone_id = var.cloudflare_zone_id
+resource "cloudflare_record" "terraform_managed_resource_4b752e4c067197db3f1cfacce3cdbdc4" {
+  content = "20.88.188.200"
+  name    = "terraformoncloudflare.com"
+  proxied = true
+  ttl     = 1
+  type    = "A"
+  zone_id = "3c7827a32e4219c64ad50cc275363d53"
+}
+
+resource "cloudflare_record" "terraform_managed_resource_d64ed9d0e34c399880d48a46dec92f5c" {
+  content = "20.88.188.200"
   name    = "terraform"
-  content = "20.88.188.200"
-  type    = "A"
   proxied = true
+  ttl     = 1
+  type    = "A"
+  zone_id = "3c7827a32e4219c64ad50cc275363d53"
 }
 
-resource "cloudflare_record" "variable-test" {
-  zone_id = var.cloudflare_zone_id
+resource "cloudflare_record" "terraform_managed_resource_95aefc2f8b708090e1b98d06c47e92f3" {
+  content = "20.88.188.200"
   name    = "variable-test"
-  content = "20.88.188.200"
-  type    = "A"
   proxied = true
+  ttl     = 1
+  type    = "A"
+  zone_id = "3c7827a32e4219c64ad50cc275363d53"
 }
 
-resource "cloudflare_record" "root" {
-  zone_id = var.cloudflare_zone_id
-  name    = "@"
-  content = "20.88.188.200"
-  type    = "A"
-  proxied = true
-}
